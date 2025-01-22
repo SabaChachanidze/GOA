621320df2df8dcad7cef3b37d7f6200631bfc41a
SabaChachanidze <chachanidzes973@Gmail.com>
2025-01-22 21:00:16 +0400

    commit

---
+++ level 70/classwork/script.js
@@ -0,0 +1,10 @@
+const firstName = "საბა";
+const lastName = "ჩაჩანიძე";
+const age = 13;
+const city = "რუსთავი";
+
+const info = `მე მქვია ${საბა} ${ჩაჩანიძე}, ვარ ${13} წლის და ვცხოვრობ ${რუსთავში}-ში.`;
+
+console.log(info);
+
+alert(info);
